93f2787ef59326c9e1b28a2f93db54fc38e68959
Jules Rivoire <julesriv@gmail.com>
2021-02-21 12:48:35 +0100

    Refacto str_converter to the norme

--- converters/str_converter.c
+++ converters/str_converter.c
@@ -6,71 +6,84 @@
 /*   By: jrivoire <jrivoire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/18 15:58:19 by jrivoire          #+#    #+#             */
-/*   Updated: 2021/02/20 18:52:41 by jrivoire         ###   ########.fr       */
+/*   Updated: 2021/02/21 14:27:03 by jrivoire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
-static void		fill_my_str(char **my_str, char filler,
-		int min_width, int right_pad)
+char			filler(t_specs specs)
 {
-	char	*str;
-	int		index;
+	char filler;
 
-	str = *my_str;
-	index = 1;
-	if (right_pad)
-	{
-		while (index <= min_width)
-			str[index++] = filler;
-	}
+	filler = (specs.zero_pad ? '0' : ' ');
+	filler = (specs.right_pad ? ' ' : filler);
+	return (filler);
+}
+
+static void		calculate_len_str(t_specs specs, char *str, int *len_str)
+{
+	if (specs.precision != -1)
+		if (specs.precision < (int)ft_strlen(str))
+			*len_str = specs.precision;
+		else
+			*len_str = (int)ft_strlen(str);
 	else
+		*len_str = (int)ft_strlen(str);
+}
+
+void			right_pad_fill(char **my_str, char *str,
+		int width, t_specs specs)
+{
+	int index;
+	int len_str;
+
+	index = 0;
+	calculate_len_str(specs, str, &len_str);
+	while (str[index] && index < len_str)
 	{
-		while (min_width >= 0)
-			str[min_width--] = filler;
+		(*my_str)[index] = str[index];
+		index++;
 	}
-	return ;
+	while (index < width)
+		(*my_str)[index++] = filler(specs);
+	(*my_str)[index] = 0;
+}
+
+void			left_pad_fill(char **my_str, char *str,
+		int padding, t_specs specs)
+{
+	int index;
+	int len_str;
+
+	index = 0;
+	calculate_len_str(specs, str, &len_str);
+	while (padding-- > 0)
+		(*my_str)[index++] = filler(specs);
+	padding = 0;
+	while (str[padding] && padding < len_str)
+		(*my_str)[index++] = str[padding++];
+	(*my_str)[index] = 0;
 }
 
 char			*str_converter(t_specs specs, char *str)
 {
 	char	*my_str;
-	char	filler;
 	int		width;
 	int		padding;
-	int		index;
 	int		len_str;
 
 	if (!str)
 		return (ft_strdup("(null)"));
-	filler = (specs.zero_pad ? '0' : ' ');
-	filler = (specs.right_pad ? ' ' : filler);
-	len_str = specs.precision < ft_strlen(str) ? specs.precision : ft_strlen(str);
+	calculate_len_str(specs, str, &len_str);
 	padding = specs.min_f_width - len_str;
 	width = (padding < 0 ? len_str : specs.min_f_width);
 	my_str = malloc(sizeof(*my_str) * (width + 1));
 	if (!my_str)
 		return (NULL);
-	index = 0;
 	if (specs.right_pad)
-	{
-		while (str[index] && index < len_str)
-		{
-			my_str[index] = str[index];
-			index++;
-		}
-		while (index < width)
-			my_str[index++] = filler;
-	}
+		right_pad_fill(&my_str, str, width, specs);
 	else
-	{
-		while (padding-- > 0)
-			my_str[index++] = filler;
-		padding = 0;
-		while (str[padding] && padding < len_str)
-			my_str[index++] = str[padding++];
-	}
-	my_str[index] = 0;
+		left_pad_fill(&my_str, str, padding, specs);
 	return (my_str);
 }
