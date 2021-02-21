cfa8cf70b2f69fa2200d9abbdccc6ff22d8b3b05
Jules Rivoire <julesriv@gmail.com>
2021-02-21 18:10:13 +0100

    Add string_writer function

--- string_writer.c
+++ string_writer.c
@@ -1,31 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*   string_writer.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jrivoire <jrivoire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/02/18 15:12:18 by jrivoire          #+#    #+#             */
-/*   Updated: 2021/02/21 17:49:33 by jrivoire         ###   ########.fr       */
+/*   Created: 2021/02/21 17:30:29 by jrivoire          #+#    #+#             */
+/*   Updated: 2021/02/22 12:23:58 by jrivoire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFTPRINTF_H
-# define LIBFTPRINTF_H
+#include "libftprintf.h"
 
-# include <stdlib.h>
-# include <unistd.h>
-# include "libft.h"
-
-typedef	struct		s_specs
+int		string_writer(char **dest, char *to_add)
 {
-	int	zero_pad;
-	int	right_pad;
-	int	min_f_width;
-	int	precision;
-}					t_specs;
+	char *temp;
 
-char	*char_converter(t_specs specs, int c);
-char	*str_converter(t_specs specs, char *str);
-int		string_writer(char **dest, char *to_add);
-#endif
+	temp = *dest;
+	*dest = ft_strjoin(*dest, to_add);
+	if (!(*dest))
+		return (0);
+	free(to_add);
+	free(temp);
+	return (1);
+}
