0342cdcb68614b9370e0938ec404f32a0704d399
Jules Rivoire <julesriv@gmail.com>
2021-02-23 17:25:53 +0100

    Adding ptn_converter

--- headers/libftprintf.h
+++ headers/libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: jrivoire <jrivoire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/18 15:12:18 by jrivoire          #+#    #+#             */
-/*   Updated: 2021/02/23 13:46:31 by jrivoire         ###   ########.fr       */
+/*   Updated: 2021/02/23 16:27:10 by jrivoire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,7 @@ typedef	struct		s_specs
 }					t_specs;
 
 char				*char_converter(t_specs specs, int c);
+char				*ptn_converter(t_specs specs, unsigned int c);
 char				*str_converter(t_specs specs, char *str);
 int					converting_function(t_specs specs, va_list *arguments,
 											char **dest);
