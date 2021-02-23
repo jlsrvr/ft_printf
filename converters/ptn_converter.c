3a7207bf71cd225fb5c1da009df71414b4a0aa27
Jules Rivoire <julesriv@gmail.com>
2021-02-23 19:45:30 +0100

    WIP ptn converter pass easy test

--- converters/ptn_converter.c
+++ converters/ptn_converter.c
@@ -6,13 +6,21 @@
 /*   By: jrivoire <jrivoire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/23 15:06:22 by jrivoire          #+#    #+#             */
-/*   Updated: 2021/02/23 16:25:47 by jrivoire         ###   ########.fr       */
+/*   Updated: 2021/02/23 19:42:59 by jrivoire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
+char	*hexa_base = "0123456789abcdef";
+
 char	*ptn_converter(t_specs specs, unsigned int ptn)
 {
-	return (ft_itoa(ptn));
+	char *hexa_trans;
+	char *my_string;
+
+	my_string = ft_strdup("0x10");
+	hexa_trans = ft_itoa_base(ptn, hexa_base);
+	string_writer(&my_string, hexa_trans);
+	return (my_string);
 }
