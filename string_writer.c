867089e0c498ee2c475aeb7d43e928ab11933195
Jules Rivoire <julesriv@gmail.com>
2021-02-25 14:08:01 +0100

    Free both string if malloc fails in string_writer

--- string_writer.c
+++ string_writer.c
@@ -6,7 +6,7 @@
 /*   By: jrivoire <jrivoire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/21 17:30:29 by jrivoire          #+#    #+#             */
-/*   Updated: 2021/02/22 12:23:58 by jrivoire         ###   ########.fr       */
+/*   Updated: 2021/02/25 14:06:56 by jrivoire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,9 @@ int		string_writer(char **dest, char *to_add)
 
 	temp = *dest;
 	*dest = ft_strjoin(*dest, to_add);
-	if (!(*dest))
-		return (0);
 	free(to_add);
 	free(temp);
+	if (!(*dest))
+		return (0);
 	return (1);
 }
