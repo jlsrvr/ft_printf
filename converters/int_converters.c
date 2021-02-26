f8a57b2149f53f3798a0e5d87cdc61c23b590c37
Jules Rivoire <julesriv@gmail.com>
2021-02-26 18:34:25 +0100

    Change int converters

--- converters/int_converters.c
+++ converters/int_converters.c
@@ -6,15 +6,16 @@
 /*   By: jrivoire <jrivoire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/25 19:38:47 by jrivoire          #+#    #+#             */
-/*   Updated: 2021/02/25 23:11:05 by jrivoire         ###   ########.fr       */
+/*   Updated: 2021/02/26 18:33:05 by jrivoire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
-char	*hexa_converter(t_specs specs, int c)
+void	hexa_converter(t_specs specs, int c, int *count)
 {
 	char			*base_x;
+	char			*my_str;
 	unsigned int	u_int;
 
 	u_int = (unsigned int)c;
@@ -22,23 +23,34 @@ char	*hexa_converter(t_specs specs, int c)
 		base_x = "0123456789abcdef";
 	else
 		base_x = "0123456789ABCDEF";
-	return (base_converter(specs, u_int, base_x));
+	my_str = base_converter(specs, u_int, base_x);
+	*count += ft_strlen(my_str);
+	ft_putstr_fd(my_str, 1);
+	free(my_str);
 }
 
-char	*unsigned_converter(t_specs specs, int c)
+void	unsigned_converter(t_specs specs, int c, int *count)
 {
 	char			*base10;
+	char			*my_str;
 	unsigned int	u_int;
 
 	base10 = "0123456789";
 	u_int = (unsigned int)c;
-	return (base_converter(specs, u_int, base10));
+	my_str = base_converter(specs, u_int, base_10);
+	*count += ft_strlen(my_str);
+	ft_putstr_fd(my_str, 1);
+	free(my_str);
 }
 
-char	*signed_converter(t_specs specs, int c)
+void	signed_converter(t_specs specs, int c, int *count)
 {
 	char			*base10;
+	char			*my_str;
 
 	base10 = "0123456789";
-	return (base_converter(specs, c, base10));
+	my_str = base_converter(specs, u_int, base_10);
+	*count += ft_strlen(my_str);
+	ft_putstr_fd(my_str, 1);
+	free(my_str);
 }
--- headers/libftprintf.h
+++ headers/libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: jrivoire <jrivoire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/18 15:12:18 by jrivoire          #+#    #+#             */
-/*   Updated: 2021/02/26 18:06:26 by jrivoire         ###   ########.fr       */
+/*   Updated: 2021/02/26 18:32:15 by jrivoire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,15 +28,10 @@ typedef	struct		s_specs
 	char			format;
 }					t_specs;
 
-void				char_converter(t_specs specs, int c, int *count);
 char				*fill_end(t_specs specs, size_t len_str);
-void				base_converter(t_specs specs, intmax_t c, char *base);
-void				hexa_converter(t_specs specs, int c, int *count);
+char				*base_converter(t_specs specs, intmax_t c, char *base);
 char				*oneline_free(char *to_free);
 void				ptn_converter(t_specs specs, uintmax_t c, int *count);
-void				signed_converter(t_specs specs, int c, int *count);
-void				str_converter(t_specs specs, char *str, int *count);
-void				unsigned_converter(t_specs specs, int c, int *count);
 char				filler(t_specs spec);
 void				converting_function(t_specs specs, va_list *arguments,
 											int *count);
@@ -45,6 +40,11 @@ int					string_writer(char **dest, char *to_add);
 int                 ft_printf(const char *string, ...);
 t_specs				conversion_parser(char **string, va_list *arguments);
 t_specs				initialize_specs(void);
+void				char_converter(t_specs specs, int c, int *count);
+void				hexa_converter(t_specs specs, int c, int *count);
+void				signed_converter(t_specs specs, int c, int *count);
+void				str_converter(t_specs specs, char *str, int *count);
+void				unsigned_converter(t_specs specs, int c, int *count);
 void				string_parser(char **string, va_list *arguments,
 									int *count);
 #endif
